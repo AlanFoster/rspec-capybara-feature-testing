baa28f25e905665415b86591ba98f093f56482a2
Alan Foster <alan@shopkeep.com>
2015-10-11 17:38:32 +0100

    Remove before each blocks

--- website/spec/features/blogs_spec.rb
+++ website/spec/features/blogs_spec.rb
@@ -1,19 +1,16 @@
 require 'feature_helper'
 
 feature 'Form Examples', type: :feature do
-  before(:each) do
-    visit '/'
-    click_on 'Form Examples'
-  end
-
   scenario 'Successfully creating a new blog' do
+    visit '/'
+
+    click_on 'Form Examples'
     expect(page).to have_content('Create Blog')
 
     fill_in 'blog_title', with: 'My Blog Title'
     fill_in 'blog_text', with: 'My new blog text'
 
     click_on 'Save Blog'
-
     expect(page).to have_selector('.blog--show')
 
     expect(page).to have_content('My Blog Title')
@@ -21,6 +18,9 @@ feature 'Form Examples', type: :feature do
   end
 
   scenario 'Entering no data' do
+    visit '/'
+    click_on 'Form Examples'
+
     expect(page).to have_content('Create Blog')
 
     click_on 'Save Blog'
