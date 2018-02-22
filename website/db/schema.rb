3515a72378b193e02298396f7a4f50bd73cf897c
Alan Foster <alan@alanfoster.me>
2018-02-22 22:39:04 +0000

    Specify rails migration version

--- website/db/schema.rb
+++ website/db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,9 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20151004125635) do
 
-  create_table "blogs", force: true do |t|
-    t.string   "title"
-    t.text     "text"
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
